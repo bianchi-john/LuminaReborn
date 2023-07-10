696123b20c667262a251cd8368c739d9ee80fe14
john-lenci-bianchi <john.lenci.bianchi@gmail.com>
2023-07-10 18:20:24 +0200

    Developed the first version of search functionality (general search)

--- Front-end/scripts/search.js
+++ Front-end/scripts/search.js
@@ -19,8 +19,45 @@ function toggleAdvancedSearch() {
         advancedSearchButton.style.borderColor = "#007bff"; 
     }
 }
+function handleSearch() {
+    // Ottieni il valore inserito nell'input di ricerca
+    var query = document.getElementById('generalSearchBar').value;
+    
+    // Controlla se la stringa di ricerca è vuota
+    if (query.trim() === '') {
+      // La stringa di ricerca è vuota, non effettuare la chiamata GET
+      console.log('La stringa di ricerca è vuota.');
+      return;
+    }
+    
+    // Crea l'URL per la chiamata GET
+    var url = 'http://0.0.0.0:3000/search/?queryString=' + encodeURIComponent(query);
+    
+    // Effettua la chiamata GET
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        // La chiamata ha avuto successo, visualizza il risultato nella pagina
+        var response = xhr.responseText;
+        // Fai qualcosa con la risposta, ad esempio:
+        document.getElementById('result').innerHTML = response;
+      } else {
+        // La chiamata non è riuscita, gestisci l'errore di conseguenza
+        console.error('Errore nella chiamata GET: ' + xhr.status);
+      }
+    };
+    
+    xhr.send();
+  }
 
 $(document).ready(function () {
     var button = document.getElementById("advancedSearchButton");
     button.addEventListener("click", toggleAdvancedSearch);
+    var generalSearchButton = document.getElementById('generalSearchButton');
+    generalSearchButton.addEventListener('click', handleSearch);
 });
+
+
+
