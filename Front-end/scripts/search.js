85a8307045c16bb92ca9986c037c7cfa02cef4e3
john-lenci-bianchi <john.lenci.bianchi@gmail.com>
2023-11-16 11:54:54 +0100

    restored local docker addess

--- Front-end/scripts/search.js
+++ Front-end/scripts/search.js
@@ -54,8 +54,8 @@ function retrieveSuggestion() {
   });
 
   const urls = [
-    "http://10.180.53.210:5000/materiali",
-    "http://10.180.53.210:5000/tecniche"
+    "http://0.0.0.0:3000/materiali",
+    "http://0.0.0.0:3000/tecniche"
   ];
 
   const risultati = {};
@@ -307,7 +307,7 @@ function handleSearch() {
 
   document.getElementById('result').innerHTML = '';
 
-  var url = 'http://10.180.53.210:5000/search/?';
+  var url = 'http://0.0.0.0:3000/search/?';
   var queries = [];
 
   if (advancedSearchFields.style.display === "none") {
